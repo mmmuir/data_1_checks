c98a23cfc0f1d54afe100e1c728e47bd5d2626e7
Chris <christophermbennett@protonmail.com>
2022-05-23 01:16:00 -0400

    added hello world and changed some variable names

--- knowledge_check_1.py
+++ knowledge_check_1.py
@@ -1,14 +1,15 @@
 #!
+greeting = "hello world!"
 
+print(greeting)
 
-turtles = "turtles"
-
-secret_message = ["while it's", True, f"that {turtles}", False, "sandwich", {
+mixed_list = ["while it's", True, f"that I just printed {greeting}, it's not ", False, "that I'm about to print a dictionary value", {
     "porridge": 39.3, "chimichangas": "good"}]
 requested_dictionary = {
     "my_first_value": (4234.43, 85.64),
     "not_my_first_value": (4324.45, 85.40)}
 your_tuple = (43, 32, 49, 40)
 print(your_tuple[0])
-print(list(secret_message[5])[1])
+print(list(mixed_list[5])[1])
 print(requested_dictionary["my_first_value"])
+ 
