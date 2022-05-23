3ababb21f8e141e129d946a87051a020fa814162
Chris <christophermbennett@protonmail.com>
2022-05-23 01:21:19 -0400

    removed empty shebang

--- knowledge_check_1.py
+++ knowledge_check_1.py
@@ -1,14 +1,12 @@
-#!
 greeting = "hello world!"
-
-print(greeting)
-
 mixed_list = ["while it's", True, f"that I just printed {greeting}, it's not ", False, "that I'm about to print a dictionary value", {
     "porridge": 39.3, "chimichangas": "good"}]
 requested_dictionary = {
     "my_first_value": (4234.43, 85.64),
     "not_my_first_value": (4324.45, 85.40)}
 your_tuple = (43, 32, 49, 40)
+
+print(greeting)
 print(your_tuple[0])
 print(list(mixed_list[5])[1])
 print(requested_dictionary["my_first_value"])
