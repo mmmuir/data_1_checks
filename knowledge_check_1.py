1bc02ff8ea0d84371631e513c032778c77551930
Chris <christophermbennett@protonmail.com>
2022-05-23 00:16:55 -0400

    initial commit

---
+++ knowledge_check_1.py
@@ -0,0 +1,14 @@
+#!
+
+
+turtles = "turtles"
+
+secret_message = ["while it's", True, f"that {turtles}", False, "sandwich", {
+    "porridge": 39.3, "chimichangas": "good"}]
+requested_dictionary = {
+    "my_first_value": (4234.43, 85.64),
+    "not_my_first_value": (4324.45, 85.40)}
+your_tuple = (43, 32, 49, 40)
+print(your_tuple[0])
+print(list(secret_message[5])[1])
+print(requested_dictionary["my_first_value"])
